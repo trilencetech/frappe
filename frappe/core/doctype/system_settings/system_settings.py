f05f4e0f1616d9b0e1aac72de63abf908e8d4d53
trilencetech <trilencetechnology@gmail.com>
2026-01-09 10:08:28 +0530

    Update Restricted Time from System Settings

--- frappe/core/doctype/system_settings/system_settings.py
+++ frappe/core/doctype/system_settings/system_settings.py
@@ -26,6 +26,7 @@ class SystemSettings(Document):
 		allow_login_using_user_name: DF.Check
 		allow_older_web_view_links: DF.Check
 		allowed_file_extensions: DF.SmallText | None
+		allowed_ips: DF.Text | None
 		app_name: DF.Data | None
 		apply_strict_user_permissions: DF.Check
 		attach_view_link: DF.Check
@@ -35,9 +36,7 @@ class SystemSettings(Document):
 		country: DF.Link | None
 		currency: DF.Link | None
 		currency_precision: DF.Literal["", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-		date_format: DF.Literal[
-			"yyyy-mm-dd", "dd-mm-yyyy", "dd/mm/yyyy", "dd.mm.yyyy", "mm/dd/yyyy", "mm-dd-yyyy"
-		]
+		date_format: DF.Literal["yyyy-mm-dd", "dd-mm-yyyy", "dd/mm/yyyy", "dd.mm.yyyy", "mm/dd/yyyy", "mm-dd-yyyy"]
 		default_app: DF.Literal[None]
 		deny_multiple_sessions: DF.Check
 		disable_change_log_notification: DF.Check
@@ -55,9 +54,8 @@ class SystemSettings(Document):
 		enable_telemetry: DF.Check
 		enable_two_factor_auth: DF.Check
 		encrypt_backup: DF.Check
-		first_day_of_the_week: DF.Literal[
-			"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-		]
+		end_block_time: DF.Data | None
+		first_day_of_the_week: DF.Literal["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 		float_precision: DF.Literal["", "2", "3", "4", "5", "6", "7", "8", "9"]
 		force_user_to_reset_password: DF.Int
 		force_web_capture_mode_for_uploads: DF.Check
@@ -70,20 +68,9 @@ class SystemSettings(Document):
 		logout_on_password_reset: DF.Check
 		max_auto_email_report_per_user: DF.Int
 		max_file_size: DF.Int
-		minimum_password_score: DF.Literal["2", "3", "4"]
 		max_report_rows: DF.Int
-		number_format: DF.Literal[
-			"#,###.##",
-			"#.###,##",
-			"# ###.##",
-			"# ###,##",
-			"#'###.##",
-			"#, ###.##",
-			"#,##,###.##",
-			"#,###.###",
-			"#.###",
-			"#,###",
-		]
+		minimum_password_score: DF.Literal["2", "3", "4"]
+		number_format: DF.Literal["#,###.##", "#.###,##", "# ###.##", "# ###,##", "#'###.##", "#, ###.##", "#,##,###.##", "#,###.###", "#.###", "#,###"]
 		otp_issuer_name: DF.Data | None
 		password_reset_limit: DF.Int
 		rate_limit_email_link_login: DF.Int
@@ -92,6 +79,7 @@ class SystemSettings(Document):
 		rounding_method: DF.Literal["Banker's Rounding (legacy)", "Banker's Rounding", "Commercial Rounding"]
 		session_expiry: DF.Data | None
 		setup_complete: DF.Check
+		start_block_time: DF.Data | None
 		store_attached_pdf_document: DF.Check
 		strip_exif_metadata_from_uploaded_images: DF.Check
 		time_format: DF.Literal["HH:mm:ss", "HH:mm"]
