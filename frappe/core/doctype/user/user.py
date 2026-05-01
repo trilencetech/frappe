b1258eb44b28b0e239c71e2fc2f2d16a6a7c1387
trilencetech <trilencetechnology@gmail.com>
2026-05-01 11:33:35 +0530

    Update Paper Stock for Self

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -118,6 +118,7 @@ class User(Document):
 		new_password: DF.Password | None
 		notifications: DF.Check
 		onboarding_status: DF.SmallText | None
+		party_customer: DF.Link | None
 		phone: DF.Data | None
 		redirect_url: DF.SmallText | None
 		reset_password_key: DF.Data | None
